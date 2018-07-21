666c50f4d7d623a983fa2570484b3ecd8cce6f21
Polcius <polcius@gmail.com>
2018-07-21 14:09:38 +0200

    refactor to ES6 class properties

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -57,19 +57,15 @@ SelectLanguage.propTypes = {
 };
 
 class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       selectedLanguage: "All",
       repos: null
-    };
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
+    }
 
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage);
   }
-  updateLanguage(lang) {
+  updateLanguage = (lang) => {
     this.setState(() => ({
     
         selectedLanguage: lang,
